1f8f9af0473ccd573c6c9d40b0c29b4cbab33f1e
FreeUser <for.free@rambler.ru>
2015-10-08 23:04:16 +0300

    Refactor. Add big and small news\docs. Add search in windows.location for share. Add docs for ubuntu wifi hotspot

--- js/CoreController.js
+++ js/CoreController.js
@@ -2,8 +2,37 @@ app.controller("CoreController",
 [ "$scope", "$http", function($scope, $http) {
 
   $http.get("content/docs.json").success(function(response){
-        $scope.news = response;
-    });
-  $scope.isTarget = false;
+    $scope.docs = response;
+  });
 
+  $scope.getQueryParams = function (qs) {
+    qs = qs.split("+").join(" ");
+    var params = {}, tokens,
+    re = /#[?&]?([^=]+)=([^&]*)/g;
+    while (tokens = re.exec(qs)) {
+      params[decodeURIComponent(tokens[1])]
+      = decodeURIComponent(tokens[2]);
+    }
+    return params;
+  }
+
+  $scope.clickDoc = function(url) {
+    $scope.isTarget = true;
+    $scope.docUrl = url;
+  }
+
+  $scope.backDoc = function() {
+    $scope.isTarget = false;
+  }
+
+  $scope.searchAny = function() {
+    var urlParams = $scope.getQueryParams(document.location.hash);
+    $scope.search = urlParams["s"];
+  }
+
+  $scope.changeSearch = function(search) {
+    document.location.hash = "?s=" + search;
+  }
+
+  $scope.searchAny();
 }]);
