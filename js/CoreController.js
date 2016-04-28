884cf1cf076af68fdca15c60a30387611d09e109
FreeUser <for.free@rambler.ru>
2016-04-28 15:11:55 +0300

    Add target post by id

--- js/CoreController.js
+++ js/CoreController.js
@@ -1,8 +1,20 @@
 app.controller("CoreController",
-[ "$scope", "$http", function($scope, $http) {
+[ "$scope", "$http", "$filter", function($scope, $http, $filter) {
 
   $http.get("content/docs.json").success(function(response){
     $scope.docs = response;
+    if($scope.target != null) {
+      var doc = $filter('filter')($scope.docs, function (d) {return d.id === ($scope.target - 0);})[0];
+      for(var i  = 0; i < doc.buttons.length; i++) {
+        var btn = doc.buttons[i];
+        if(btn.isExternal) {
+
+        } else {
+          $scope.clickDoc(btn.url, doc.id);
+          break;
+        }
+      }
+    }
   });
 
   $scope.getQueryParams = function (qs) {
@@ -16,23 +28,36 @@ app.controller("CoreController",
     return params;
   }
 
-  $scope.clickDoc = function(url) {
+  $scope.clickDoc = function(url, id) {
     $scope.isTarget = true;
     $scope.docUrl = url;
+    $scope.target = id;
+    setActualHash();
   }
 
   $scope.backDoc = function() {
     $scope.isTarget = false;
+    setActualHash();
   }
 
-  $scope.searchAny = function() {
+  var searchAny = function() {
     var urlParams = $scope.getQueryParams(document.location.hash);
     $scope.search = urlParams["s"];
+    $scope.target = urlParams["target"]
+  }
+
+  var setActualHash = function() {
+    if($scope.isTarget) {
+      document.location.hash = "?target=" + $scope.target;
+    } else {
+      document.location.hash = $scope.search ? "?s=" + $scope.search : "";
+    }
   }
 
   $scope.changeSearch = function(search) {
-    document.location.hash = "?s=" + search;
+    $scope.search = search;
+    setActualHash();
   }
 
-  $scope.searchAny();
+  searchAny();
 }]);
